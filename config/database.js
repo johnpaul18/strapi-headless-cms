99ce3aff081eb1ac3e4a82087a4870def4f49d6e
John Paul Alimango <esze0909@gmail.com>
2021-08-26 19:55:40 +0800

    :

--- config/database.js
+++ config/database.js
@@ -1,36 +1,15 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: "default",
-//   connections: {
-//     default: {
-//       connector: "mongoose",
-//       settings: {
-//         host: env("DATABASE_HOST"),
-//         srv: env.bool("DATABASE_SRV", true),
-//         port: env.int("DATABASE_PORT", 27017),
-//         database: env("DATABASE_NAME"),
-//         username: env("DATABASE_USERNAME"),
-//         password: env("DATABASE_PASSWORD"),
-//       },
-//       options: {
-//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-//         ssl: env.bool("DATABASE_SSL", true),
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster0.see7q.mongodb.net"),
+        host: env("DATABASE_HOST"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "headless-cms"),
-        username: env("DATABASE_USERNAME", "john-paul"),
-        password: env("DATABASE_PASSWORD", "925619123"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
